851585c714f6ca110fe2992ee6420af9d39fab00
Vishwa <67421829+hosathota@users.noreply.github.com>
2021-10-19 14:18:11 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using IBM.Data.Db2;
+
+namespace DockerTest6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            try
+            {
+                DB2Connection connection = new DB2Connection("server=test;");
+
+                connection.Open();
+                Console.WriteLine("zOS Connection opened with server " + connection.ServerVersion);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error in opening connection "+ex.Message);
+            }
+
+            Console.WriteLine("completed without error");
+        }
+    }
+}
