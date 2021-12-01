3195c3608fc5308d581ee67da416385d622f20e8
Vishwa <67421829+hosathota@users.noreply.github.com>
2021-12-01 12:21:13 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,29 +10,29 @@ namespace DockerTester
         static void Main(string[] args)
         {
             Console.WriteLine("Starting db2 app 18th Nov");
-            string path = Directory.GetCurrentDirectory();
-            Console.WriteLine("The current directory is {0}", path);
+            //string path = Directory.GetCurrentDirectory();
+            //Console.WriteLine("The current directory is {0}", path);
             
-            string[] directories = Directory.GetDirectories(path);
+            //string[] directories = Directory.GetDirectories(path);
 
-                foreach(string dir in directories)
-                {
-                    Console.WriteLine("The sub directory is {0}", dir);
-                }
+                //foreach(string dir in directories)
+                //{
+                    //Console.WriteLine("The sub directory is {0}", dir);
+                //}
             
-            Console.WriteLine("Before setting : LD_LIBRARY_PATH is {0}", Environment.GetEnvironmentVariable("LD_LIBRARY_PATH"));
+            //Console.WriteLine("Before setting : LD_LIBRARY_PATH is {0}", Environment.GetEnvironmentVariable("LD_LIBRARY_PATH"));
             
-            Environment.SetEnvironmentVariable("LD_LIBRARY_PATH", path+@"/clidriver/lib");
+            //Environment.SetEnvironmentVariable("LD_LIBRARY_PATH", path+@"/clidriver/lib");
             
-            Console.WriteLine("The LD_LIBRARY_PATH is {0}", Environment.GetEnvironmentVariable("LD_LIBRARY_PATH"));
+            //Console.WriteLine("The LD_LIBRARY_PATH is {0}", Environment.GetEnvironmentVariable("LD_LIBRARY_PATH"));
             
-            ProcessStartInfo startInfo = new ProcessStartInfo() { FileName = "/bin/bash", Arguments = "ls", };
-                Process proc = new Process() { StartInfo = startInfo, };
-                proc.Start();
+            //ProcessStartInfo startInfo = new ProcessStartInfo() { FileName = "/bin/bash", Arguments = "ls", };
+                //Process proc = new Process() { StartInfo = startInfo, };
+                //proc.Start();
             
             try
             {
-                DB2Connection connection = new DB2Connection("server=dddd:454545;uid=def;pwd=fdfd;database=defg;SECURITY=SSL;");
+                DB2Connection connection = new DB2Connection("server=myserver:50000;uid=myuid;pwd=mypwd;database=mydb;SECURITY=SSL;");
 
                 connection.Open();
                 Console.WriteLine("Connection opened with server " + connection.ServerVersion);
